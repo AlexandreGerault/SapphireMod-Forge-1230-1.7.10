7cf7a87e27161bd1bfdfd7f114e5ab65c2ef07c9
alexan14 <alexan14.webmaster@gmail.com>
2015-01-25 17:38:09 +0100

    Add sources
    I add the sources of the mod

---
+++ src/main/java/com/mralexan/sapphiremod/initializers/SapphireInitializers.java
@@ -0,0 +1,10 @@
+package com.mralexan.sapphiremod.initializers;
+
+public class SapphireInitializers
+{
+	public static void initialize()
+	{
+		SapphireBlocks.initializeBlocks();
+		SapphireItems.initializeItems();
+	}
+}
